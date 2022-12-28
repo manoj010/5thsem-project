e0e8ba5a776d270ffa383dab4ea57055c6d438b5
Suwas52 <suwasdanuwar18@gmail.com>
2022-12-28 23:38:29 +0545

    Admin panel Added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard() {
-        return view('admin.admin_dashboard');
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,5 @@ require __DIR__.'/auth.php';
 
 Route::middleware('auth','role:admin')->group(function() {
     Route::get('/admin/dashboard',[AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout',[AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
