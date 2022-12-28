ff614c42f326c3e91e685774bd03bdff8ed7d343
Suwas52 <suwasdanuwar18@gmail.com>
2022-12-28 15:21:14 +0545

    Dashboard Part Added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware('auth','role:admin')->group(function() {
+    Route::get('/admin/dashboard',[AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
