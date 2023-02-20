62fd33d04af6b810444d4c55476a2d9c0e098587
manoj010 <manojale100@gmail.com>
2023-02-20 21:16:38 +0545

    footer-added

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -1,9 +1,395 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <p>Copyright © 2022 <a href="index.html">SUVIDA</a></p>
-            </div>
+<!-- <footer id="footer" class="footer-1">
+<div class="main-footer widgets-dark typo-light">
+<div class="container">
+<div class="row">
+
+<div class="col-xs-12 col-sm-6 col-md-3">
+<div class="widget subscribe no-box">
+<h5 class="widget-title">COMPANY NAME<span></span></h5>
+<p>About the company, little description will goes here.. </p>
+</div>
+</div>
+
+  
+<div class="col-xs-12 col-sm-6 col-md-3">
+<div class="widget no-box">
+<h5 class="widget-title">Quick Links<span></span></h5>
+<ul class="thumbnail-widget">
+<li>
+<div class="thumb-content"><a href="#.">&nbsp;Get Started</a></div> 
+</li>
+<li>
+<div class="thumb-content"><a href="#.">&nbsp;Top Leaders</a></div> 
+</li>
+<li>
+</ul>
+</div>
+</div>
+
+  
+
+      <div class="col-xs-12 col-sm-6 col-md-3">
+<div class="widget no-box">
+<h5 class="widget-title">Follow up<span></span></h5>
+            <a href="#"> <i class="fa fa-facebook"> </i> </a>
+            <a href="#"> <i class="fa fa-twitter"> </i> </a>
+            <a href="#"> <i class="fa fa-youtube"> </i> </a>
+</div>
+</div>
+<br>
+  <br>
+
+
+<div class="col-xs-12 col-sm-6 col-md-3">
+<div class="widget no-box">
+<h5 class="widget-title">Contact Us<span></span></h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+  <div class="emailfield">
+<input type="text" name="email" value="Email">
+<input name="uri" type="hidden" value="arabiantheme">
+<input name="loc" type="hidden" value="en_US">
+<input class="submitbutton ripplelink" type="submit" value="Subscribe">
+</form>  
+</div>
+</div>
+
+</div>
+</div>
+</div>
+  
+<div class="footer-copyright">
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-center">
+<p>Copyright Design Sherif Hamdy © 2019. All rights reserved.</p>
+</div>
+</div>
+</div>
+</div>
+</footer> -->
+<style>
+    .round-btn {display: inline;height: 40px; width: 40px; background:#fff;border-radius: 50%;float: left;margin: 15px 8px;box-shadow: 2px 2px 5px 0px rgb(82, 0, 67);border: 1px solid;/*border: 1px solid #622657;*/}
+    .round-btn a {display: block !important;padding: 7px 12px;font-size: 18px;border-radius: 50%;}
+    .round-btn .icon {padding: 3px;}
+    .round-btn .icon img{height: 24px; width: 32px;margin-top: 6px;}
+    .btn-facebook a {color: #3b5998;padding: 8px 13px;}
+    .btn-linkedin a {color: #007bb6;}
+    .btn-twitter a{color: #1c9deb;}
+    .btn-instagram a{color: #dd3f5c;}
+    .btn-whatsapp a{color: #155E54;}
+    .btn-envelop a{color: #D6403A;font-size: 15px; padding: 9px 12px;}
+    .standard_header .standard_social_links {margin-left: 1rem;}
+
+    /*
+    =========================================================================================
+                                        footer
+    =========================================================================================
+    */
+    
+    .footer-wrap {
+        padding-top: 43px;
+        background-size: cover;
+    }
+
+    .footer-wrap h3 {
+        color: #fff;
+        font-size: 17px;
+        text-transform: uppercase;
+        margin-bottom: 25px;
+    }
+
+    .footer-wrap p {
+        font-size: 13px;
+        line-height: 24px;
+        color: #b3b3b3;
+        margin-top: 15px;
+    }
+
+    .footer-wrap p a {
+        color: #fff;
+        text-decoration: underline;
+        font-style: italic;
+    }
+
+    .footer-wrap p a:hover {
+        text-decoration: none;
+        color: #ff7800;
+    }
+
+    .footer-links li a {
+        font-size: 13px;
+        line-height: 30px;
+        color: #ccc;
+        text-decoration: none;
+    }
+
+
+    .footer-links li:before {
+        content: "\f105";
+        font-family: 'FontAwesome';
+        padding-right: 10px;
+        color: #ccc;
+    }
+
+    .footer-category li a {
+        font-size: 13px;
+        line-height: 30px;
+        color: #ccc;
+        text-decoration: none;
+    }
+
+    .footer-category li:before {
+        content: "\f105";
+        font-family: 'FontAwesome';
+        padding-right: 10px;
+        color: #b3b3b3;
+    }
+
+    .address {
+        
+        color: #b3b3b3;
+        font-size: 14px;
+        position: relative;
+        padding-left: 30px;
+        line-height: 30px;
+    }
+
+    .address:before {
+        content: "\f277";
+        font-family: 'FontAwesome';
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+
+    .info a {
+    
+        color: #b3b3b3;
+        font-size: 14px;
+        line-height: 30px;
+        font-weight: normal;
+    }
+
+    .fa-phone:before {
+        content: "\f095";
+    }
+
+    .info a {
+
+        color: #b3b3b3;
+        font-size: 14px;
+        line-height: 30px;
+        font-weight: normal;
+    }
+
+    .fa-fax:before {
+        content: "\f1ac";
+    }
+
+    .copyright {
+        border-top: 1px solid #111;
+        font-size: 14px;
+        color: #ccc;
+        padding-top: 15px;
+        text-align: center;
+        padding-bottom: 15px;
+        background: #222;
+    }
+    footer .second_class{
+        border-bottom: 1px solid #444;
+        padding-bottom: 25px;
+    }
+    footer .first_class {
+        padding-bottom: 21px;
+        border-bottom: 1px solid #444;
+    }
+    footer .first_class p, footer .first_class h3{
+        margin: 0 0;
+        
+    }
+    footer{
+        background: #333;
+        padding: 30px 0px 0px 0px;
+    }
+
+    footer .newsletter input[type="text"] {
+        width: 100%;
+        background: #fff;
+        color: #333;
+        border: 1px solid #222;
+        padding: 14px 20px;
+        border-radius: 50px;
+        margin-top: 12px;
+    }
+
+    .newsletter .newsletter_submit_btn {
+        background: #fff;
+        position: absolute;
+        right: 30px;
+        border: 0;
+        top: 26px;
+        font-size: 17px;
+        color: #333;
+    }
+
+
+    footer .second_class_bdr{
+        padding-top: 25px;
+        border-top:1px solid #222;
+    }
+
+    footer .btn-facebook a {
+        padding: 6px 14px !important;
+    }
+
+    footer .btn-envelop a {
+        color: #D6403A;
+        font-size: 15px;
+        padding: 12px 12px;
+    }
+
+    footer .round-btn a {
+        padding: 6px 12px;
+    }
+
+    footer .round-btn {
+        box-shadow: 2px 2px 5px 0px #222 !important;}
+
+    footer .round-btn {
+        margin: 15px 4px;}
+    
+    footer dl, ol, ul {
+        padding-left: 5px;
+    }
+    footer li{
+        list-style: none;
+    }
+
+    @media(max-width:768px){
+        .footer-wrap h3 {
+        margin-top: 27px;}
+        
+        footer .round-btn {
+        margin: 15px 4px;}
+    }
+    @media(max-width:320px){
+    .copyright {
+        font-size: 13px;}
+    } 
+</style>
+
+<footer class="container-fluid">
+  <div class="footer-wrap">
+  <div class="container first_class">
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+          <h3>BE THE FIRST TO KNOW</h3>
+          <p>Get all the latest information on  Triedge Services, Events, Jobs
+            and Fairs. Sign up for our newsletter today.</p>
         </div>
+        <div class="col-md-4 col-sm-6">
+        <form class="newsletter">
+           <input type="text" placeholder="Email Address"> 
+            <button class="newsletter_submit_btn" type="submit"><i class="fa fa-paper-plane"></i></button>  
+        </form>
+        
+        </div>
+        <div class="col-md-4 col-sm-6">
+        <div class="col-md-12">
+          <div class="standard_social_links">
+        <div>
+          <li class="round-btn btn-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a>
+
+          </li>
+          <li class="round-btn btn-linkedin"><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a>
+
+          </li>
+          <li class="round-btn btn-twitter"><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+
+          </li>
+          <li class="round-btn btn-instagram"><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+
+          </li>
+          <li class="round-btn btn-whatsapp"><a href="#"><i class="fab fa-whatsapp" aria-hidden="true"></i></a>
+
+          </li>
+          <li class="round-btn btn-envelop"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+
+          </li>
+        </div>
+      </div>  
+        </div>
+          <div class="clearfix"></div>
+        <div class="col-md-12"><h3 style="text-align: right;">Stay Connected</h3></div>
+        </div>
+      </div>
+  </div>
+    <div class="second_class">
+      <div class="container second_class_bdr">
+      <div class="row">
+        <div class="col-md-4 col-sm-6">
+
+          <div class="footer-logo"><img src="http://localhost/lrn/img/footer_logo.png" alt="logo">
+          </div>
+          <p>Your one-stop career platform to find Jobs, Internships, Professional Trainings, Projects, and Volunteering Opportunities.</p>
+        </div>
+        <div class="col-md-2 col-sm-6">
+          <h3>Quick  LInks</h3>
+          <ul class="footer-links">
+            <li><a href="#">Home</a>
+            </li>
+            <li><a href="#">About us</a>
+            </li>
+            <li><a href="#">Triedge Partners</a>
+            </li>
+            <li><a href="#">Contact Us</a>
+            </li>
+            <li><a href="#" target="_blank">Terms &amp; Conditions</a>
+            </li>
+            <li><a href="#" target="_blank">Privacy Policy</a>
+            </li>
+            <li><a href="#">Sitemap</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h3>OUR SERVICES</h3>
+          <ul class="footer-category">
+            <li><a href="#">Fresher Jobs</a>
+            </li>
+            <li><a href="#">InternEdge - Internships &amp; Projects </a>
+            </li>
+            <li><a href="#">Resume Edge - Resume Writing Services</a>
+            </li>
+            <li><a href="#">Readers Galleria - Curated Library</a>
+            </li>
+            <li><a href="#">Trideus - Campus Ambassadors</a>
+            </li>
+          </ul>
+          <div class="clearfix"></div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+          <h3>triedge Events</h3>
+          <ul class="footer-links">
+            <li><a href="#">Triedge Events</a>
+            </li>
+
+            <li><a href="#">Jobs &AMP; Internship Fair 2019</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
     </div>
-</footer>
+    </div>
+    
+    <div class="row">
+      
+      <div class="container-fluid">
+        <div class="copyright"> Copyright 2019 | All Rights Reserved by TRIEDGE Solutions Pvt. Ltd.</div>
+      </div>
+
+    </div>
+  </div>
+  
+  </footer>
